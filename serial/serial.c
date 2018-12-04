52f6f1ec5a54834474a0c0613a6dc16638be2c7a
justin <justin.martin.developper@gmail.com>
2018-12-04 14:38:27 +0100

    serial try

--- serial/serial.c
+++ serial/serial.c
@@ -4,64 +4,47 @@
 #define FOSC 16000000
 
 void setup (void);
-void blinkMyLed();
 void init_USART(unsigned int baud);
-int printCHAR(char character, FILE *stream);
-
-int printCHAR(char character, FILE *stream)
-{
-	while ((UCSRA & (1 << UDRE)) == 0) {};
-
-	UDR = character;
-
-	return 0;
-}
+void USART_SendByte(uint8_t u8Data);
 
 FILE uart_str = FDEV_SETUP_STREAM(printCHAR, NULL, _FDEV_SETUP_RW);
 
-void init_USART (unsigned int baud) {
-	int UBRR;
-	UBRR = (int) ((FOSC / 16 / baud) - 1);
-	
-	UBRR0H = (unsigned char)(UBRR>>8);
-	UBRR0L = (unsigned char)UBRR;
-	/* Enable receiver and transmitter */
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	/* Set frame format: 8data, 1stop bit */
-	UCSR0C = (1<<USBS0)|(2<<UCSZ00);
-	
-	fprintf(&uart_str, "Hello World\n\r");
-	stdout = &uart_str;
-	printf("I'm init\n\r");
+void Serial_INIT (unsigned int baud) {
+	BAUD_PRESCALE = (((FOSC / (baud * 16UL))) - 1)
+	UBRRL = BAUD_PRESCALE;
+	UBRRH = (BAUD_PRESCALE >> 8); 
 
+	UCSRB = ((1<<TXEN)|(1<<RXEN) | (1<<RXCIE));
 }
 
-
+/*
 void blinkMyLed() {
-		PORTD |= 0b00010000; //allumer only D4
-		_delay_ms (250);
-		
-		
-		
-		PORTD &= 0b11101111; //eteindre D
-		_delay_ms (250);
+	PORTD |= 0b00010000; //allumer only D4
+	_delay_ms (250);
+	PORTD &= 0b11101111; //eteindre D
+	_delay_ms (250);
+}*/
+
+
+void USART_SendByte(uint8_t u8Data){
+
+  // Wait until last byte has been transmitted
+  while((UCSRA &(1<<UDRE)) == 0);
+
+  // Transmit data
+  UDR = u8Data;
 }
 
 
-
-
-void setup (void)
-{	
-	DDRD = 0b01110000; // broche D4
-	PORTD = 0b00000000; //Port D for D4 
+void setup (void) {	
 	init_USART(9600);
 }
 
-int main (void)
-{
+int main (void) {
 	setup();
 	while(1){	
-		blinkMyLed();
+		USART_SendByte(value);  // send value 
+		_delay_ms(250);         
 	}
 	
 	return 0;
