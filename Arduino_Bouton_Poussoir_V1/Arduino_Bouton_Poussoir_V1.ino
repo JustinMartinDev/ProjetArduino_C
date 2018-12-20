ae19e2df8fd67456dadd4c3e6cdd678877226b4b
BarthMarsault <barth2803@gmail.com>
2018-12-20 12:07:21 +0100

    Correction Arduino Bouton et Timer

--- Arduino_Bouton_Poussoir_V1/Arduino_Bouton_Poussoir_V1.ino
+++ Arduino_Bouton_Poussoir_V1/Arduino_Bouton_Poussoir_V1.ino
@@ -1,53 +1,52 @@
 #include <avr/interrupt.h>
-#define LED_D3 3 //Led sur le port D3
+
 #define LED_D4 4 //Led sur le port D4
+#define LED_D5 5 //Led sur le port D5
 #define BUTTON_D2 2 //Bouton sur le port D2
 
 volatile byte state = LOW;
-int first;
 
 void setup() {
-  first = 0;
   
-  pinMode(LED_D3, OUTPUT);
   pinMode(LED_D4, OUTPUT);
+  pinMode(LED_D5, OUTPUT);
   pinMode(BUTTON_D2, INPUT_PULLUP);
-  interrupts(); //Autorise les interruptions
-  digitalWrite(LED_D4, LOW);
+
   attachInterrupt(digitalPinToInterrupt(BUTTON_D2), blink ,RISING); //Execute la fonction blink() lors de l'appui sur le bouton
 
-
+  interrupts();
   TCCR1A = 0b00000000;//Initialisation du mode de fonctionnement du timer
-  TCCR1B = 0b00000101;//Initialisation de la fréquence du registre
+  TCCR1B = 0b00000101;//Initialisation de la frequence du registre
   TIMSK1 = 0b00000001;
-  TCNT1 =  49910; //65535 -(1/64*10^6) : 1000ms
- 
+  TCNT1 =  49910; //65535 -(1/64*10^6) : 1000ms //compteur initialisé
+  
+
+
 }
 
+
+/*function déclancher à chaque interruption */
 ISR(TIMER1_OVF_vect){
-  noInterrupts(); //Bloque les interruptions
-  digitalWrite(LED_D3, !digitalRead(LED_D3)); //Change l'état de la LED_D3
+  noInterrupts();//Bloque les interruptions
+  
+  digitalWrite(LED_D4, !digitalRead(LED_D4)); //Change l'état de la LED
   
   TIFR1 &= 0b00000000;
 
-  //Modification de la valeur de registre de comptage 
-  //Afin de modifier le temps des cycles de clignotement
-  if(first%2 == 0) TCNT1 = 49910;
-  else TCNT1 = 60000;
-  first++;
+  TCNT1 = 49910;
   
   TIMSK1 |= 0b00000001;
-  interrupts(); //Autorise les interruptions
+  
+  interrupts();//Autorise les interruptions
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_D4, state);
+  digitalWrite(LED_D5, state); //Change l'état de la LED_D5 selon la variable state
 }
 
 void blink(){
-    //noInterrupts();
-    state = !state;
-    //digitalWrite(LED_D4, !digitalRead(LED_D4)); //Change l'état de la LED_D4
-    //interrupts();
+    state = !state; //Change l'état de state (HIGH/LOW)
 }
